3348614a144d61d04890ba1930486756dfab0e36
Yanrishatum <krabanek@gmail.com>
2022-01-25 13:34:30 +0300

    Fix input_scalar_n, fix state storage float

--- imgui/ImGui.hx
+++ imgui/ImGui.hx
@@ -601,15 +601,15 @@ class ImStateStorage
 	static function state_storage_set_int(storage: ImStateStoragePtr, id: ImGuiID, val: Int): Void {}
 	static function state_storage_get_bool(storage: ImStateStoragePtr, id: ImGuiID, default_val: Bool): Bool { return false; }
 	static function state_storage_set_bool(storage: ImStateStoragePtr, id: ImGuiID, val: Bool): Void {}
-	static function state_storage_get_float(storage: ImStateStoragePtr, id: ImGuiID, default_val: Float): Float { return 0; }
-	static function state_storage_set_float(storage: ImStateStoragePtr, id: ImGuiID, val: Float): Void {}
+	static function state_storage_get_float(storage: ImStateStoragePtr, id: ImGuiID, default_val: Single): Single { return 0; }
+	static function state_storage_set_float(storage: ImStateStoragePtr, id: ImGuiID, val: Single): Void {}
 	
 	public inline function setInt(id: ImGuiID, val: Int):Void state_storage_set_int(ptr, id, val);
 	public inline function getInt(id: ImGuiID, default_val: Int = 0):Int return state_storage_get_int(ptr, id, default_val);
 	public inline function setBool(id: ImGuiID, val: Bool):Void state_storage_set_bool(ptr, id, val);
 	public inline function getBool(id: ImGuiID, default_val: Bool = false):Bool return state_storage_get_bool(ptr, id, default_val);
-	public inline function setFloat(id: ImGuiID, val: Float):Void state_storage_set_float(ptr, id, val);
-	public inline function getFloat(id: ImGuiID, default_val: Float = 0.0):Float return state_storage_get_float(ptr, id, default_val);
+	public inline function setFloat(id: ImGuiID, val: Single):Void state_storage_set_float(ptr, id, val);
+	public inline function getFloat(id: ImGuiID, default_val: Single = 0.0):Single return state_storage_get_float(ptr, id, default_val);
 }
 
 
